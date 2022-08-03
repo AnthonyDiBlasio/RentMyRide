064de292f4960325db51c31c0acb71566daa2cbc
Anthony DiBlasio <anthony.diblasio11@gmail.com>
2022-08-03 18:05:31 -0400

    adding boostrap

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
