b29f744a35d26d6916250fac66fdec5fb46605e0
Anthony DiBlasio <anthony.diblasio11@gmail.com>
2022-08-08 17:34:41 -0400

    adding changes to front end

---
+++ client/src/components/NavTabs.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
+
+
+
+import React from 'react';
+
+// TODO: Add a comment explaining how we are able to extract the key value pairs from props
+
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#home"
+          onClick={() => handlePageChange('Home')}
+          //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
+
+          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          //  TODO: Add a comment explaining what this logic is doing
+
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#signup"
+          onClick={() => handlePageChange('Signup')}
+          //  TODO: Add a comment explaining what this logic is doing
+
+          className={currentPage === 'Signup' ? 'nav-link active' : 'nav-link'}
+        >
+          Projects
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#Search"
+          //  TODO: Add a comment explaining what this logic is doing
+
+          onClick={() => handlePageChange('Search')}
+          className={currentPage === 'Search' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
