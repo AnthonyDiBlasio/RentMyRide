40da5f76acc9c85a83ff31c8d9c77de053a9e691
cpaschall <cpaschall216@gmail.com>
2022-08-12 20:36:46 -0400

    refactoring auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -29,8 +29,6 @@ module.exports = {
       // return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    // next();
     return req;
   },
   signToken: function ({ username, email, _id }) {
