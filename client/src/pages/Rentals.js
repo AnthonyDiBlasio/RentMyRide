dbf90ded85ce2926ca32a4fce1a114c2eb26b46e
Anthony DiBlasio <anthony.diblasio11@gmail.com>
2022-08-17 14:28:22 -0400

    adding packages to update versions of all dependent packages also adding package for mdb for styling cards

--- client/src/pages/Rentals.js
+++ client/src/pages/Rentals.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { useQuery } from '@apollo/client';
 import { QUERY_CAR } from '../utils/queries';
-import { Button } from "bootstrap";
+import {
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCardImage,
+    MDBBtn
+  } from 'mdb-react-ui-kit';
 
 const Rentals = () => {
     const { loading, data } = useQuery(QUERY_CAR, {
@@ -14,31 +21,42 @@ const Rentals = () => {
     const carList = data?.cars || [];
 
     return (
-        <div className="rentContainer card bg-white card-rounded w-70">
-            <div className="card-header bg-dark text-center">
+        <div className="rentContainer ">
+            <div className="container">
                 <h1>RentmyRide</h1>
             </div>
-            <div className="rentCard card-body m-5">
+            <div className="rentCard ">
                 <h2>Get into Gear</h2>
-                <ul className="carlist">
+                <ul className="carlist"> 
                     {carList.map((car) => {
                         return (
+                          
                             
-                            <ul className="carCard card bg-white card-rounded w-60" key={car._id}>
+                            <MDBCard>
+                            <MDBCardImage src={car.image} position='top' alt='...' />
+                            <MDBCardBody>
+                              <MDBCardTitle>Available for Rent</MDBCardTitle>
+                              <MDBCardText key={car._id}>
+                               
+                            
                                 <span className="span">Type: {car.carType}</span><br />
                                 <span className="span"> Make: {car.carMake}</span><br />
                                 <span className="span"> Model: {car.carModel}</span><br />
                                 <span className="span"> Year: {car.carYear}</span><br />
                                 <span className="span"> Color: {car.color}</span><br />
-                                <img className ="img-fluid"src = {car.image}/>
-                                <span className="span">Price per day: ${car.price}</span><br /><span><button> book this rental</button></span>
-                            </ul>
-                            
+                                <span className="span">Price per day: ${car.price}</span><br /><span></span>
+                                
+                                
+                              </MDBCardText>
+                              <MDBBtn href='#'>Book this car</MDBBtn>
+                            </MDBCardBody>
+                          </MDBCard>
                             
 
 
                         );
                     })}
+                    
                 </ul>
             </div>
         </div>
