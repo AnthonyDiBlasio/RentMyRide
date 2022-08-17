369399f6a652fe40c531f9786c988b7b84477d4b
Anthony DiBlasio <anthony.diblasio11@gmail.com>
2022-08-17 16:17:26 -0400

    making CarCard.js under components and refactoring rentals.js... still need to get images to render, also need to put filter into rentals.js

--- client/src/pages/Rentals.js
+++ client/src/pages/Rentals.js
@@ -1,66 +1,9 @@
 import React from "react";
-import { useQuery } from '@apollo/client';
-import { QUERY_CAR } from '../utils/queries';
-import {
-    MDBCard,
-    MDBCardBody,
-    MDBCardTitle,
-    MDBCardText,
-    MDBCardImage,
-    MDBBtn
-  } from 'mdb-react-ui-kit';
+import CarCard from "../components/CarCard";
 
 const Rentals = () => {
-    const { loading, data } = useQuery(QUERY_CAR, {
-        fetchPolicy: "no-cache"
-    });
+return(<CarCard/>)
 
-    // const [cars, setCars] = useState({
-
-    // })
-    const carList = data?.cars || [];
-
-    return (
-        <div className="rentContainer ">
-            <div className="container">
-                <h1>RentmyRide</h1>
-            </div>
-            <div className="rentCard ">
-                <h2>Get into Gear</h2>
-                <ul className="carlist"> 
-                    {carList.map((car) => {
-                        return (
-                          
-                            
-                            <MDBCard>
-                            <MDBCardImage src={car.image} position='top' alt='...' />
-                            <MDBCardBody>
-                              <MDBCardTitle>Available for Rent</MDBCardTitle>
-                              <MDBCardText key={car._id}>
-                               
-                            
-                                <span className="span">Type: {car.carType}</span><br />
-                                <span className="span"> Make: {car.carMake}</span><br />
-                                <span className="span"> Model: {car.carModel}</span><br />
-                                <span className="span"> Year: {car.carYear}</span><br />
-                                <span className="span"> Color: {car.color}</span><br />
-                                <span className="span">Price per day: ${car.price}</span><br /><span></span>
-                                
-                                
-                              </MDBCardText>
-                              <MDBBtn href='#'>Book this car</MDBBtn>
-                            </MDBCardBody>
-                          </MDBCard>
-                            
-
-
-                        );
-                    })}
-                    
-                </ul>
-            </div>
-        </div>
-    );
 };
 
 
