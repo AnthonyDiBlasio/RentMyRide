1a36b5b876313c2c16e8ba72275b50f578c55d75
choyoonme <yoonme@gmail.com>
2022-08-17 22:37:28 -0400

    refactored navtabs to use react bootstrap components

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -1,77 +1,53 @@
-import React from 'react';
-import '../styles/NavTabs.css';
-import { Link} from 'react-router-dom';
-import { LoneSchemaDefinitionRule } from 'graphql';
+import React from "react";
+import "../styles/NavTabs.css";
+
 // returns the navbar on everypage
-
-function NavTabs({ currentPage, handlePageChange }) {
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { useLocation } from "react-router-dom";
+export default function NavTabs({ currentPage, handlePageChange }) {
+  const location = useLocation();
   return (
-    <ul className="nav nav-tabs justify-content-end navColor">
-      <li className="nav-item ">
-        <Link
-          to="/"
-          
-
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/signup"
-    
-          className={currentPage === 'Signup' ? 'nav-link active' : 'nav-link'}
-        >
-        Signup
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/login"
-        
-          
-
-          className={currentPage === 'Signup' ? 'nav-link active' : 'nav-link'}
-        >
-        Login
-        </Link>
-      </li>
-      <li className="nav-item">
-        <a
-          href="/logout"
-          onClick={() => handlePageChange('Signup')}
-          
-
-          className={currentPage === 'Signup' ? 'nav-link active' : 'nav-link'}
-        >
-        Logout
-        </a>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/rentals"
-        
-
-          
-          className={currentPage === 'Rentals' ? 'nav-link active' : 'nav-link'}
-        >
-          Find your Dream Ride!
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/rentalform"
-         
-          
-
-          className={currentPage === 'RentalForm' ? 'nav-link active' : 'nav-link'}
-        >
-          Rent your Ride!
-        </Link>
-      </li>
-    </ul>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">Rent My Ride</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/rentals" active={location.pathname === "/rentals"}>
+              Find a Ride
+            </Nav.Link>
+            <Nav.Link
+              href="/rentalform"
+              active={location.pathname === "/rentalform"}
+            >
+              List a Ride
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/signup" active={location.pathname === "/signup"}>
+              Signup
+            </Nav.Link>
+            <Nav.Link href="/login" active={location.pathname === "/login"}>
+              Login
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Navbar.Text>
+              <a
+                href="/logout"
+                onClick={() => handlePageChange("Signup")}
+                className={
+                  currentPage === "Signup" ? "nav-link active" : "nav-link"
+                }
+              >
+                Logout
+              </a>
+            </Navbar.Text>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
-
-export default NavTabs;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,58 +1,68 @@
-import React, {useEffect, useReducer} from 'react';
-import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_ME } from '../utils/queries';
-import Auth from '../utils/auth';
-import { useUser } from '../context/UserContext';
-import reducer from '../context/reducers';
-import {LOGIN, LOGOUT} from '../context/actions';
+import React, { useEffect, useReducer } from "react";
+import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_ME } from "../utils/queries";
+import Auth from "../utils/auth";
+import { useUser } from "../context/UserContext";
+import reducer from "../context/reducers";
+import { LOGIN, LOGOUT } from "../context/actions";
 
-import '../styles/Home.css';
+import "../styles/Home.css";
 
 const Home = () => {
   const initialState = useUser();
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const logout = (event) =>{
+  const logout = (event) => {
     event.preventDefault();
 
     Auth.logout(dispatch);
-  }
-
-  
+  };
 
   const { loading: loadingMe, data: dataMe } = useQuery(QUERY_ME, {
-    fetchPolicy: "no-cache"
+    fetchPolicy: "no-cache",
   });
 
   const me = dataMe?.me || {};
 
-  useEffect( () => {
-    if(me && me.hasOwnProperty("_id")){
-      if(state.user === null || me._id !== state.user._id ){
-        dispatch({type: LOGIN, payload: me});
+  useEffect(() => {
+    if (me && me.hasOwnProperty("_id")) {
+      if (state.user === null || me._id !== state.user._id) {
+        dispatch({ type: LOGIN, payload: me });
       }
     }
   }); // want to update state on any change
   return (
-    <div className='container-fluid d-flex flex-wrap flex-column align-content-center'>
+    <div className="container-fluid d-flex flex-wrap flex-column align-content-center">
       {/* header section */}
-      <div className='carImgHeader mb-3'>
-        <div className='headerText'>
-          <h1 className='heading1Home p-5'>Get Into Gear and Find Your Dream Ride</h1>
-          <h2 className='heading2Home text-center'>Book Online Now</h2>
-        </div>  
+      <div className="carImgHeader mb-3">
+        <div className="headerText">
+          <h1 className="heading1Home p-5">
+            Get Into Gear and Find Your Dream Ride
+          </h1>
+          <h2 className="heading2Home text-center">Book Online Now</h2>
+        </div>
       </div>
 
       {/* not liking this section at Home.js. Maybe put it on about.js? */}
-      <div className=' card homeInfo' style={{ width: '50rem'}}>
-        <div className='card-body m-3'>
-          <h3 className='card-title'>When you rent from us:</h3>
-          <ul className='list-group list-group-flush'>
-            <li class="list-group-item">24/7 Customer Support - Here for any issues that might arrise during your trip.</li>
-            <li class="list-group-item">Pick your dream car - Countless makers and models to chosen from.</li>
-            <li class="list-group-item">Rent and Earn - Rent your AMAZING car to others and earn money while doing it.</li>
-            <li class="list-group-item">Drive with Confidence - comes with standard third-party insurance.</li>
+      <div className="card homeInfo " style={{ width: "80rem" }}>
+        <div className="card-body m-3">
+          <h3 className="card-title ">How it works</h3>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              24/7 Customer Support - Here for any issues that might arise
+              during your trip.
+            </li>
+            <li className="list-group-item">
+              Pick your dream car - Countless makers and models to chosen from.
+            </li>
+            <li className="list-group-item">
+              Rent and Earn - Rent your amazing car to others and earn money
+              while doing it.
+            </li>
+            <li className="list-group-item">
+              Drive with Confidence - Comes with standard third-party insurance.
+            </li>
           </ul>
         </div>
       </div>
