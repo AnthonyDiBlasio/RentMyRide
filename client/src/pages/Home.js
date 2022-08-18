6be6fc9ad818accfeb0c3b711a09f91dbf2fd484
choyoonme <yoonme@gmail.com>
2022-08-17 22:46:27 -0400

    fix home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,49 +1,50 @@
-import React, { useEffect, useReducer } from "react";
-import { Link } from "react-router-dom";
-import { useQuery } from "@apollo/client";
-import { QUERY_ME } from "../utils/queries";
-import Auth from "../utils/auth";
-import { useUser } from "../context/UserContext";
-import reducer from "../context/reducers";
-import { LOGIN, LOGOUT } from "../context/actions";
+import React, {useEffect, useReducer} from 'react';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_ME } from '../utils/queries';
+import Auth from '../utils/auth';
+import { useUser } from '../context/UserContext';
+import reducer from '../context/reducers';
+import {LOGIN, LOGOUT} from '../context/actions';
+
+import '../styles/Home.css';
 
 const Home = () => {
   const initialState = useUser();
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const logout = (event) => {
+  const logout = (event) =>{
     event.preventDefault();
 
     Auth.logout(dispatch);
-  };
+  }
+
+  
 
   const { loading: loadingMe, data: dataMe } = useQuery(QUERY_ME, {
-    fetchPolicy: "no-cache",
+    fetchPolicy: "no-cache"
   });
 
   const me = dataMe?.me || {};
 
-  useEffect(() => {
-    if (me && me.hasOwnProperty("_id")) {
-      if (state.user === null || me._id !== state.user._id) {
-        dispatch({ type: LOGIN, payload: me });
+  useEffect( () => {
+    if(me && me.hasOwnProperty("_id")){
+      if(state.user === null || me._id !== state.user._id ){
+        dispatch({type: LOGIN, payload: me});
       }
     }
   }); // want to update state on any change
   return (
-    <div className="container-fluid d-flex flex-wrap flex-column align-content-center">
+    <div className='container-fluid d-flex flex-wrap flex-column align-content-center'>
       {/* header section */}
-      <div className="carImgHeader mb-3">
-        <div className="headerText">
-          <h1 className="heading1Home p-5">
-            Get Into Gear and Find Your Dream Ride
-          </h1>
-          <h2 className="heading2Home text-center">Book Online Now</h2>
-        </div>
+      <div className='carImgHeader mb-3'>
+        <div className='headerText'>
+          <h1 className='heading1Home p-5'>Get Into Gear and Find Your Dream Ride</h1>
+          <h2 className='heading2Home text-center'>Book Online Now</h2>
+        </div>  
       </div>
 
       {/* not liking this section at Home.js. Maybe put it on about.js? */}
-
       <div className=' card homeInfo' style={{ width: '50rem'}}>
         <div className='card-body m-3'>
           <h3 className='card-title'>When you rent from us:</h3>
