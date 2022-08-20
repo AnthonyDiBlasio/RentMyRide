6d49f551f08dff2a5c15aff68b86f4291b3b3f13
Sajjad Zaidi <sajjadzaidi2015@gmail.com>
2022-08-20 13:05:29 -0400

    refactoring mutation and delete mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,5 +26,52 @@ mutation login($email: String!, $password: String!) {
   }
 }
 `;
+export const CREATE_CAR = gql`
+mutation createCar($carType: String!, $carMake: String!, $carModel: String!, $carYear: Int!, $price: Int!, $isAvailable: Boolean!, $color: String, $locationAvail: String, $carOwner: ID) {
+  createCar(carType: $carType, carMake: $carMake, carModel: $carModel, carYear: $carYear, price: $price, isAvailable: $isAvailable, color: $color, locationAvail: $locationAvail, carOwner: $carOwner) {
+    _id
+    carType
+    carMake
+    carModel
+    carYear
+    color
+    price
+    isAvailable
+    locationAvail
+    carOwner {
+      _id
+      name
+      email
+      location
+    }
+  }
+}
+`;
+export const CREATE_BOOKING = gql`
+mutation createBooking($rentedCar: ID, $reservDate: String, $returnDate: String, $totalBill: Int, $billingDate: String, $lateFee: Int, $message: String) {
+  createBooking(rentedCar: $rentedCar, reservDate: $reservDate, returnDate: $returnDate, totalBill: $totalBill, billingDate: $billingDate, lateFee: $lateFee, message: $message) {
+    _id
+    rentedCar {
+      _id
+      carType
+      carMake
+      carModel
+      carYear
+      color
+      price
+      image
+      isAvailable
+      locationAvail
+    }
+    reservDate
+    returnDate
+    totalBill
+    billingDate
+    lateFee
+    message
+  }
+}
+`
+
 
 
