e18071c7dadabe793274bf5f043db9ec4df4cfee
choyoonme <yoonme@gmail.com>
2022-08-21 17:22:40 -0400

    style margins

--- client/src/pages/FindMyRide.js
+++ client/src/pages/FindMyRide.js
@@ -5,6 +5,7 @@ import { QUERY_CAR } from "../utils/queries";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const FindMyRide = () => {
   const [carTypeFilter, setCarTypeFilter] = useState([]);
@@ -38,63 +39,83 @@ const FindMyRide = () => {
   //render form input for on change set is available true or false checkbox
 
   return (
-    <div>
-      <h1>Find My Ride</h1>
-      <Form.Group as={Col} controlId="my_multiselect_field">
-        <Form.Label>Desired Car Type</Form.Label>
-        <Form.Control
-          as="select"
-          multiple
-          value={carTypeFilter}
-          onChange={(e) => {
-            const selection = Array.from(e.target.selectedOptions).map(
-              (item) => item.value
-            );
-            setCarTypeFilter(selection);
+    <div style={{ display: "flex" }}>
+      <div>
+        <Card
+          style={{
+            width: "300px",
+            padding: "16px",
+            position: "sticky",
+            top: "0",
+            margin: "16px",
           }}
         >
-          {[
-            "Convertible",
-            "Roadster",
-            "Sedan",
-            "Coupe",
-            "SUV",
-            "Hatchback",
-          ].map((carType) => (
-            <option key={carType} value={carType}>
-              {carType}
-            </option>
-          ))}
-        </Form.Control>
-      </Form.Group>
-      <Form.Label>Start date:</Form.Label>
-      <Form.Control
-        type="date"
-        name="startDate"
-        onChange={(e) => {
-          setStartDate(e.target.value);
-        }}
-      />
-      <Form.Label>End date:</Form.Label>
-      <Form.Control
-        type="date"
-        name="endDate"
-        onChange={(e) => {
-          setEndDate(e.target.value);
-        }}
-      />
+          <Card.Title style={{ textAlign: "center", fontSize: "30px" }}>
+            Find My Ride
+          </Card.Title>
 
-      <Button
-        variant="primary"
-        onClick={() => {
-          console.log(
-            "TODO! This onclick event should query backend for cars that match the date range"
-          );
-          console.log(startDate, endDate);
-        }}
-      >
-        Search
-      </Button>
+          <Form.Group as={Col} controlId="my_multiselect_field">
+            <Form.Label style={{ textAlign: "left" }}>Car Type</Form.Label>
+            <Form.Control
+              as="select"
+              multiple
+              value={carTypeFilter}
+              onChange={(e) => {
+                const selection = Array.from(e.target.selectedOptions).map(
+                  (item) => item.value
+                );
+                console.log(selection);
+                setCarTypeFilter(selection);
+              }}
+            >
+              {[
+                "Convertible",
+                "Roadster",
+                "Sedan",
+                "Coupe",
+                "SUV",
+                "Hatchback",
+              ].map((carType) => (
+                <option key={carType} value={carType}>
+                  {carType}
+                </option>
+              ))}
+            </Form.Control>
+            <p style={{ textAlign: "left", fontSize: "10px" }}>
+              to select multiple types hit control click
+            </p>
+          </Form.Group>
+          <Form.Label style={{ marginTop: "16px" }}>Start date:</Form.Label>
+          <Form.Control
+            type="date"
+            name="startDate"
+            onChange={(e) => {
+              setStartDate(e.target.value);
+            }}
+          />
+          <Form.Label style={{ marginTop: "16px" }}>End date:</Form.Label>
+          <Form.Control
+            type="date"
+            name="endDate"
+            onChange={(e) => {
+              setEndDate(e.target.value);
+            }}
+          />
+
+          <Button
+            style={{ marginTop: "16px" }}
+            variant="primary"
+            onClick={() => {
+              console.log(
+                "TODO! This onclick event should query backend for cars that match the date range"
+              );
+              console.log(startDate, endDate);
+            }}
+          >
+            Search
+          </Button>
+        </Card>
+      </div>
       <CarCardList cars={filterCars(data.cars)} />
     </div>
   );
