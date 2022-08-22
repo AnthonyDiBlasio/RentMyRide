b905ef1b8ac89d9f73281288332a61ecd8c12147
Vincent Tjia <vtjia51@gmail.com>
2022-08-22 15:39:09 -0400

    got logged in user's name on the profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,89 +1,18 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import { useQuery } from '@apollo/client';
-import { QUERY_USERS } from '../utils/queries';
+import { QUERY_ME } from '../utils/queries';
 
 import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn, MDBTypography } from 'mdb-react-ui-kit';
 import img1 from '../images/car1.jpg';
 
 const Profile = () => {
-  const { loading, data } = useQuery(QUERY_USERS, {
-    fetchPolicy: "no-cache"
-  });
+  
+  // create state for user's info
+  const { data: userData } = useQuery(QUERY_ME);
 
-  const [formState, setFormState] = useState({
-    name: "",
-    email: "",
-    password: ""
-  });
-
-  const handleInputChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-
-    // copy
-    const formValues = {...formState};
-    // modify
-    formValues[name] = value;
-    // set state
-    setFormState(formValues);
-  }
-
-  const user = data?.user || '';
+  const user = userData?.me || '';
 
   return (
-    // <div className="card bg-white card-rounded w-50">
-    //   <div className="card-header bg-dark text-center">
-    //     <h1>Users List!</h1>
-    //   </div>
-    //   <div className="card-body m-5">
-    //     <h2>Welcome</h2>
-    //     {loading ? (
-    //       <div>Loading...</div>
-    //     ) : (
-    //       <ul className="square">
-    //         {userList.map((user) => {
-    //           return (
-    //             <li key={user._id}>
-    //             <span>{user.name}</span><br />
-    //             <span>{user.email}</span>
-    //             </li>
-    //           );
-    //         })}
-    //       </ul>
-    //     )}
-    //   </div>
-    //   <div>
-    //     <form>
-    //       <label>Name</label>
-    //       <input 
-    //         type="text" 
-    //         name="name" 
-    //         placeholder='Name of person' 
-    //         onChange={handleInputChange}
-    //         value={formState.name}
-    //       />
-    //       <label>Email</label>
-    //       <input 
-    //         type="text"
-    //         name="email" 
-    //         placeholder='Email of person' 
-    //         onChange={handleInputChange}
-    //         value={formState.email}
-    //       />
-    //       <label>Password</label>
-    //       <input 
-    //         type="password" 
-    //         name="password" 
-    //         placeholder='Password' 
-    //         onChange={handleInputChange}
-    //         value={formState.password}
-    //       />
-    //     </form>
-    //   </div>
-
-    // </div>
-
     <div className="gradient-custom-2" >
     <MDBContainer className="py-5 h-100">
       <MDBRow className="justify-content-center align-items-center h-100">
@@ -93,17 +22,19 @@ const Profile = () => {
               <div className="ms-4 mt-5 d-flex flex-column" style={{ width: '150px' }}>
                 <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
                   alt="Generic placeholder image" className="mt-4 mb-2 img-thumbnail" fluid style={{ width: '150px', zIndex: '1' }} />
-                <MDBBtn outline color="dark mt-3" style={{height: '36px', overflow: 'visible'}}>
+                {/* <MDBBtn outline color="dark mt-3" style={{height: '36px', overflow: 'visible'}}>
                   Edit profile
-                </MDBBtn>
+                </MDBBtn> */}
               </div>
               <div className="ms-3" style={{ marginTop: '130px' }}>
-                <MDBTypography tag="h5">{user.name}</MDBTypography>
+                <MDBTypography tag="h5">
+                  {user.name}
+                  </MDBTypography>
                 {/* <MDBCardText>New York</MDBCardText> */}
               </div>
             </div>
-            <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
-              {/* <div className="d-flex justify-content-end text-center py-1">
+            {/* <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
+              <div className="d-flex justify-content-end text-center py-1">
                 <div>
                   <MDBCardText className="mb-1 h5">253</MDBCardText>
                   <MDBCardText className="small text-muted mb-0">Photos</MDBCardText>
@@ -116,8 +47,8 @@ const Profile = () => {
                   <MDBCardText className="mb-1 h5">478</MDBCardText>
                   <MDBCardText className="small text-muted mb-0">Following</MDBCardText>
                 </div>
-              </div> */}
-            </div>
+              </div>
+            </div> */}
             <MDBCardBody className="text-black p-4">
               {/* <div className="mb-5">
                 <p className="lead fw-normal mb-1">About</p>
@@ -129,7 +60,7 @@ const Profile = () => {
               </div> */}
               <div className="d-flex justify-content-between align-items-center mt-5 mb-4">
                 <MDBCardText className="lead fw-normal mb-0">Cars Rented:</MDBCardText>
-                <MDBCardText className="mb-0"><a href="#!" className="text-muted">Show all</a></MDBCardText>
+                {/* <MDBCardText className="mb-0"><a href="#!" className="text-muted">Show all</a></MDBCardText> */}
               </div>
               <MDBRow>
                 <MDBCol className="mb-2">
