5617abe7044fef71b23905cdc523b8cac695ff21
Vincent Tjia <vtjia51@gmail.com>
2022-08-22 16:59:34 -0400

    all data under User's model can be accessed and viewed under profile. Changed requested fields under QUERY_ME

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -29,26 +29,30 @@ const Profile = () => {
               <div className="ms-3" style={{ marginTop: '130px' }}>
                 <MDBTypography tag="h5">
                   {user.name}
-                  </MDBTypography>
-                {/* <MDBCardText>New York</MDBCardText> */}
+                </MDBTypography>
+                <MDBCardText>
+                No data for location @ the moment {user.location}
+                </MDBCardText>
               </div>
             </div>
-            {/* <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
+            <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
               <div className="d-flex justify-content-end text-center py-1">
                 <div>
-                  <MDBCardText className="mb-1 h5">253</MDBCardText>
-                  <MDBCardText className="small text-muted mb-0">Photos</MDBCardText>
+                  <MDBCardText className="small text-muted mb-0">Email</MDBCardText>
+                  <MDBCardText className="mb-1 h5">
+                  <a href={user.email} target="_blank">{user.email}</a>
+                  </MDBCardText>
                 </div>
-                <div className="px-3">
+                {/* <div className="px-3">
                   <MDBCardText className="mb-1 h5">1026</MDBCardText>
                   <MDBCardText className="small text-muted mb-0">Followers</MDBCardText>
                 </div>
                 <div>
                   <MDBCardText className="mb-1 h5">478</MDBCardText>
                   <MDBCardText className="small text-muted mb-0">Following</MDBCardText>
-                </div>
+                </div> */}
               </div>
-            </div> */}
+            </div>
             <MDBCardBody className="text-black p-4">
               {/* <div className="mb-5">
                 <p className="lead fw-normal mb-1">About</p>
